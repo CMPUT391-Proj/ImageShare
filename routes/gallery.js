5eed26efb0aed30f0f9fba07597f3bceee6c82d2
Thomas Fung <tfung@ualberta.ca>
2014-11-12 23:18:39 -0700

    made gallery.js mac compatible, included mac oracle driver

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var oracle = require('oracle');
+if (process.platform == 'linux') {
+	var oracle = require('oracle');
+}
+else if (process.platform == 'darwin') {
+	var oracle = require('oracle_mac');
+}
 
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
