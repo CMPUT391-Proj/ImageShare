6a80a1633f812b3714f72ea349fd7287980243e0
Thomas Fung <tfung@ualberta.ca>
2014-11-13 00:26:59 -0700

    added modal for registration

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/register', function(req, res) {
+	// need to get data
 });
 
 module.exports = router;
