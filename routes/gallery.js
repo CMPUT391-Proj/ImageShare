2fad3bfb0daf96f6b0d07c6e77dda2600c84504f
Jessica Yuen <jyuen@ualberta.ca>
2014-11-15 16:48:31 -0700

    Started work on POST for image uploading

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var oracleHandler = require('oracle-handler');
 var router = express.Router();
 
-/* GET home page. */
+/* GET gallery page. */
 router.get('/', function(req, res) {
   res.render('gallery', { title: 'Gallery' });
+});
+
+/* Image uploading */
+router.post('/imageUpload', function(req, res) {
+	var description = req.body.desc;
+	var subject = req.body.subject;
+	var date = req.body.date;
+	var place = req.body.place;
+
+	var insertImagesStatement = 
+		'INSERT INTO IMAGES ' +
+		'VALUES (1, \'jesstest\', 1, \'' + subject + '\', \'' + place + '\', \'' + date + '\', \'' + description + '\', null, null)';
+
+	oracleHandler.oracleQuery(insertImagesStatement);
 });
 
 module.exports = router;
