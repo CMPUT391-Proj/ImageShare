35f3fbb589689302bf6457ddc011900bb868f881
Thomas Fung <tfung@ualberta.ca>
2014-11-16 21:15:37 -0700

    made registration its own module

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var about = require('./routes/about');
 var gallery = require('./routes/gallery');
 var groups = require('./routes/groups');
+var registration = require('./routes/registration');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/', routes);
 app.use('/about', about);
 app.use('/gallery', gallery);
 app.use('/groups', groups);
+app.use('/registration', registration);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var oracleHandler = require('oracle-handler');
-var helper = require('helper/templates');
-var async = require('async');
 var router = express.Router();
 
 /* GET home page. */
@@ -25,27 +23,6 @@ router.post('/login', function(req, res) {
 		'SELECT FROM USERS (USER_NAME, PASSWORD, DATE_REGISTERED) '+
 		'VALUES (\''+username+'\', \''+password+'\', SYSTIMESTAMP)';
 	var results = oracleHandler.oracleQuery(getUsersStatement);
-});
-
-/* Registration */
-router.post('/', function(req, res) {
-	var username = req.param('username');
-	var password = req.param('password');
-	var firstname = req.param('firstname');
-	var lastname = req.param('lastname');
-	var address = req.param('address');
-	var email = req.param('email');
-	var phone = req.param('phone');
-
-	var user = helper.createUser([username, password, 'SYSTIMESTAMP']);
-	var person = helper.createPerson([username, firstname, lastname, address, email, phone]);
-	
-	oracleHandler.oracleInsert('USERS', user, function(err, results) {
-		oracleHandler.oracleInsert('PERSONS', person, function(err2, results2) {
-			req.method = 'get';
-			res.redirect('/gallery'); 
-		});
-	});
 });
 
 module.exports = router;
